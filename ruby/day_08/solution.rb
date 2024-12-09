9c586120a62ffd3aca47745b72767850e267d2b9
Srihari <srihari.kookal@clark.io>
2024-12-09 17:36:22 +0100

    day 8

---
+++ ruby/day_08/solution.rb
@@ -0,0 +1,74 @@
+class Solution
+  attr_reader :antenna_positions, :length, :breadth, :antinodes
+
+  def initialize(input_file)
+    @antenna_positions = {}
+    length = 0
+    breadth = 0
+    File.readlines(input_file).each_with_index do |line, x|
+      length += 1
+      breadth = line.chomp.length
+      line.chomp.chars.each_with_index do |char, y|
+        if char == "."
+          # puts "Blank at #{x},#{y}"
+        else
+          @antenna_positions[char] ||= []
+          @antenna_positions[char] << [x, y]
+        end
+      end
+    end
+    @length = length
+    @breadth = breadth
+    @antinodes = []
+  end
+
+  def solution_1
+    @antenna_positions.each do |antenna, positions|
+      positions.each_with_index do |position, curr_index|
+        positions.each_with_index do |other_position, other_index|
+          next if curr_index == other_index
+          find_antinode(position, other_position).tap do |antinode|
+            if antinode[0] >= 0 && antinode[0] < (@length) && antinode[1] >= 0 && antinode[1] < (@breadth)
+              @antinodes << antinode
+            end
+          end
+        end
+      end
+    end
+    print_world
+    @antinodes.uniq.length
+  end
+
+  def solution_2
+  end
+
+  def find_antinode(a,b)
+    ax, ay = a
+    bx, by = b
+    cx = bx + ((bx - ax) )
+    cy = by + ((by - ay) )
+    [cx, cy]
+  end
+
+  def print_world
+    apos = {}
+    @antenna_positions.each do |antenna, positions|
+      positions.each do |position|
+        apos[position] = antenna
+      end
+    end
+
+    @length.times do |x|
+      @breadth.times do |y|
+        if @antinodes.include?([x, y])
+          print "#"
+        elsif apos[[x, y]]
+          print apos[[x, y]]
+        else
+          print "."
+        end
+      end
+      puts
+    end
+  end
+end
--- ruby/day_08/test_solution.rb
+++ ruby/day_08/test_solution.rb
@@ -5,20 +5,22 @@ def test_result(x, y)
     puts "Test passed"
   else
     puts "Test failed, expected #{y} but got #{x}"
+    exit
   end
 end
 
 puts "Sample, Solution 1"
 sample_result = Solution.new("input_sample.txt").solution_1
-test_result(sample_result, 3749)
-
-puts "Sample, Solution 2"
-sample_result = Solution.new("input_sample.txt").solution_2
-test_result(sample_result, 11387)
+test_result(sample_result, 14)
 
 puts "Actual, Solution 1"
 result = Solution.new("input.txt").solution_1
-test_result(result, 3119088655389)
+test_result(result, 354)
+
+puts "Sample, Solution 2"
+sample_result = Solution.new("input_sample.txt").solution_2
+test_result(sample_result, 34)
+
 
 puts "Actual, Solution 2"
 result = Solution.new("input.txt").solution_2
