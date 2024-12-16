0d00872d18f9d4eb9117301d8d96281c6069318b
Srihari <srihari.kookal@clark.io>
2024-12-17 00:01:02 +0100

    day 11

--- elixir/day_11/solution.ex
+++ elixir/day_11/solution.ex
@@ -6,19 +6,18 @@ defmodule Solution do
   def parse_input(content) do
     content
     |> String.split("\n", trim: true)
-    |> Enum.with_index(fn line, row ->
+    |> Enum.map(fn line ->
       line
       |> String.split(" ")
     end)
     |> List.flatten()
   end
 
-  def apply_rule("0" = stone) do
-    "1"
+  def apply_rule("0" = _stone) do
+    ["1"]
   end
 
   def apply_rule(stone) when is_bitstring(stone) and rem(byte_size(stone), 2) == 0 do
-    # IO.inspect(stone, label: "Applying on string with even byte size")
     stone
     |> String.split_at(div(byte_size(stone), 2))
     |> Tuple.to_list()
@@ -28,35 +27,40 @@ defmodule Solution do
   end
 
   def apply_rule(stone) when is_bitstring(stone) do
-    # IO.inspect(stone, label: "Applying on string")
-    stone
-    |> String.to_integer()
-    |> Kernel.*(2024)
-    |> Integer.to_string()
+    r =
+      stone
+      |> String.to_integer()
+      |> Kernel.*(2024)
+      |> Integer.to_string()
+
+    [r]
   end
 
-  def apply_rule(stones) when is_list(stones) or is_map(stones) do
-    #  IO.inspect(stones, label: "Applying rule on")
-    Enum.map(stones, &Solution.apply_rule/1)
-  end
+  # def apply_rule(stones) when is_list(stones) or is_map(stones) do
+  #   #  IO.inspect(stones, label: "Applying rule on")
+  #   Enum.map(stones, &Solution.apply_rule/1)
+  # end
 
   def iterate_stones(file_name, iter_count) do
     {:ok, content} = read_input(file_name)
 
-    parse_input(content)
-    |> Enum.map(fn stone ->
-      IO.inspect(stone, label: "Processing")
+    start = parse_input(content) |> Enum.frequencies()
 
-      s =
-        Enum.reduce(1..iter_count, [stone], fn x, acc ->
-          IO.inspect(x)
-          List.flatten(apply_rule(acc))
+    Enum.reduce(1..iter_count, start, fn _t, iteration_result ->
+      Enum.reduce(iteration_result, %{}, fn {item, freq}, freq_count ->
+        result = apply_rule(item)
+
+        Enum.reduce(result, freq_count, fn item, acc ->
+          if(Map.has_key?(acc, item)) do
+            Map.put(acc, item, Map.get(acc, item) + freq)
+          else
+            Map.put(acc, item, freq)
+          end
         end)
+      end)
     end)
-    |> Enum.reduce(0, fn x, acc ->
-      IO.inspect(x, label: "Lenght")
-      IO.inspect(Enum.count(x), label: "Lenght")
-      acc + Enum.count(x)
+    |> Enum.reduce(0, fn {_item, freq}, acc ->
+      freq + acc
     end)
   end
 
@@ -70,6 +74,5 @@ defmodule Solution do
 end
 
 IO.inspect(Solution.solution(:part_one, "input_sample.txt"), label: "Part 1: Sample")
-IO.inspect(Solution.solution(:part_one, "input.txt"), label: "Part 1: Sample")
-IO.inspect(Solution.solution(:part_two, "input.txt"), label: "Part 1: Sample")
-# IO.inspect(Solution.solution(:part_one, :actual), label: "Part 1: Actual")
+IO.inspect(Solution.solution(:part_one, "input.txt"), label: "Part 2: Actual")
+IO.inspect(Solution.solution(:part_two, "input.txt"), label: "Part 2: Sample")
